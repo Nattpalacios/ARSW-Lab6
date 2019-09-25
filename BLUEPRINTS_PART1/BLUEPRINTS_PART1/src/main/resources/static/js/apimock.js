251d1f7ff60b711a78587df8ef8b702934ec33fb
Natalia Palacios <natalia.palacios-r@mail.escuelaing.edu.co>
2019-09-25 07:35:05 -0500

    Agregando lab

---
+++ BLUEPRINTS_PART1/BLUEPRINTS_PART1/src/main/resources/static/js/apimock.js
@@ -0,0 +1,20 @@
+apimock=(function(){
+
+	var mockdata=[];
+
+
+
+   mockdata["Nat"]=[{"name":"Nat","functions":[{"movie":{"name":"Titanic Movie","genre":"Action"},"seats":[[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true]],"date":"2018-12-18 15:30"},{"movie":{"name":"The Purge","genre":"Horror"},"seats":[[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true]],"date":"2018-12-18 15:30"}]}];
+
+   mockdata["blueprint100"]=[{"name":"blueprint100","functions":[{"movie":{"name":"mision imposible","genre":"Fiction"},"seats":[[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true,true,true]],"date":"2018-12-18 15:31"}]}];
+
+
+	return {
+		getCinemaByName:function(name,callback){
+			callback(
+				mockdata[name]
+			);
+		}
+	}
+
+})(); 
